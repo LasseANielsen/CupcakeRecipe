6f88ae5195fa6996973d38b1a0ea6dcbf62517b9
Lasse <l.cph@hotmail.com>
2018-09-20 09:51:36 +0200

    Nu med classes OG DTO MICHAEL <3

--- CupcakeProject/src/main/java/com/mycompany/cupcakeproject/CartDTO.java
+++ CupcakeProject/src/main/java/com/mycompany/cupcakeproject/CartDTO.java
@@ -2,19 +2,19 @@ package com.mycompany.cupcakeproject;
 
 public class CartDTO {
 
-    int id, user_id;
+    int id, user_cart_id;
 
     public CartDTO(int id, int user_id) {
         this.id = id;
-        this.user_id = user_id;
+        this.user_cart_id = user_id;
     }
 
     public int getId() {
         return id;
     }
 
-    public int getUser_id() {
-        return user_id;
+    public int getUser_cart_id() {
+        return user_cart_id;
     }
 
 }
