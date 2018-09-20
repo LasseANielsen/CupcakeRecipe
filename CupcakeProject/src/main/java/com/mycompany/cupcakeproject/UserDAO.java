b380efbb9619344a7ead3e66488294105cffa666
Lasse <l.cph@hotmail.com>
2018-09-20 10:48:10 +0200

    Opgave 2.3 & 2.4

---
+++ CupcakeProject/src/main/java/com/mycompany/cupcakeproject/UserDAO.java
@@ -0,0 +1,92 @@
+package com.mycompany.cupcakeproject;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UserDAO {
+
+    public UserDTO getUserById(int id) {
+        try {
+            Connection c = new DBConnector().getConnection();
+            Statement stmt = c.createStatement();
+            String query
+                    = "SELECT * "
+                    + "FROM `User` "
+                    + "WHERE `id` = '" + id + "';";
+            ResultSet res = stmt.executeQuery(query);
+            while (res.next()) {
+                int user_id = Integer.parseInt(res.getString("Id"));
+                String name = res.getString("Name");
+                String username = res.getString("Username");
+                String password = res.getString("Password");
+                CustomerOrAdmin privileges = CustomerOrAdmin.valueOf(res.getString("C/A"));
+                int balance = Integer.parseInt(res.getString("Balance"));
+                String email = res.getString("Email");
+
+                return new UserDTO(user_id, balance, name, username, password, email, privileges);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error");
+        }
+        return null;
+    }
+
+    public UserDTO getUserByUsername(String username) {
+        try {
+            Connection c = new DBConnector().getConnection();
+            Statement stmt = c.createStatement();
+            String query
+                    = "SELECT * "
+                    + "FROM `User` "
+                    + "WHERE `username` = '" + username + "';";
+            ResultSet res = stmt.executeQuery(query);
+            while (res.next()) {
+                int id = Integer.parseInt(res.getString("Id"));
+                String name = res.getString("Name");
+                String user_username = res.getString("Username");
+                String password = res.getString("Password");
+                CustomerOrAdmin privileges = CustomerOrAdmin.valueOf(res.getString("C/A"));
+                int balance = Integer.parseInt(res.getString("Balance"));
+                String email = res.getString("Email");
+
+                return new UserDTO(id, balance, name, user_username, password, email, privileges);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error");
+        }
+        return null;
+    }
+
+    public void createUser(String email, String username, String password, int balance) {
+        try {
+            Connection c = new DBConnector().getConnection();
+            Statement stmt = c.createStatement();
+            int id = getNextAvailableId();
+            String name = "user" + id;
+            String query
+                    = "INSERT INTO `User`(`id`,`name`,`username`,`password`,`C/A`,`balance`,`email`) VALUES"
+                    + "('" + id + "','" + name + "','" + username + "','" + password + "','C',+'" + balance + "','" + email + "')";
+
+            ResultSet res = stmt.executeQuery(query);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error");
+        }
+
+    }
+
+    private int getNextAvailableId() {
+        int i = 0;
+        for (i = 0; i < Integer.MAX_VALUE; i++) {
+            UserDTO user = getUserById(i);
+            if (user == null) {
+                break;
+            }
+        }
+        return i;
+    }
+}
--- CupcakeProject/src/main/java/com/mycompany/cupcakeproject/UserDTO.java
+++ CupcakeProject/src/main/java/com/mycompany/cupcakeproject/UserDTO.java
@@ -4,16 +4,17 @@ public class UserDTO {
 
     int id, balance;
 
-    String name, username, password;
+    String name, username, password, email;
 
     CustomerOrAdmin customerOrAdmin;
 
-    public UserDTO(int id, int balance, String name, String username, String password, CustomerOrAdmin customerOrAdmin) {
+    public UserDTO(int id, int balance, String name, String username, String password, String email, CustomerOrAdmin customerOrAdmin) {
         this.id = id;
         this.balance = balance;
         this.name = name;
         this.username = username;
         this.password = password;
+        this.email = email;
         this.customerOrAdmin = customerOrAdmin;
     }
 
@@ -36,10 +37,13 @@ public class UserDTO {
     public String getPassword() {
         return password;
     }
+    
+    public String getEmail() {
+        return email;
+    }
 
     public CustomerOrAdmin getCustomerOrAdmin() {
         return customerOrAdmin;
     }
 
-    
 }
