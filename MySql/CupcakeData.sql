d9cbf84dbbf1b124b7a0ac39ad68c796598b0fe4
Michael <mich561d@hotmail.com>
2018-10-03 10:59:29 +0200

    billeder

--- MySql/CupcakeData.sql
+++ MySql/CupcakeData.sql
@@ -1,17 +1,20 @@
 INSERT INTO `Bottom` (`Id`, `Price`, `Name`) VALUES
-(101, 5, 'Chocolate'), 
-(102, 5, 'Vanilla'),
-(103, 5, 'Nutmeg'),
-(104, 6, 'Pistacio'),
-(105, 7, 'Almond');
+(1, 5, 'Chocolate'), 
+(2, 5, 'Vanilla'),
+(3, 5, 'Nutmeg'),
+(4, 6, 'Pistacio'),
+(5, 7, 'Almond');
 
 INSERT INTO `Toppings` (`Id`, `Price`, `Name`)  VALUES
-(201, 5, 'Chocolate'),
-(202, 5, 'Blueberry'),
-(203, 5, 'Rasberry'),
-(204, 6, 'Crispy'),
-(205, 6, 'Strawberry'),
-(206, 7, 'Rum/Raisin'),
-(207, 8, 'Orange'),
-(208, 8, 'Lemon'),
-(209, 9, 'Blue cheese')
+(1, 5, 'Chocolate'),
+(2, 5, 'Blueberry'),
+(3, 5, 'Rasberry'),
+(4, 6, 'Crispy'),
+(5, 6, 'Strawberry'),
+(6, 7, 'Rum/Raisin'),
+(7, 8, 'Orange'),
+(8, 8, 'Lemon'),
+(9, 9, 'Blue cheese');
+
+INSERT INTO `User` (`Id`, `Name`, `Username`, `Password`, `C/A`, `Balance`, `Email`) VALUES
+('1', 'Admin', 'Admin', '1234', 'A', '0', 'Admin@Admin.Admin');
